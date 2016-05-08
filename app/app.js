4fa632d832c0580eee26a5b485d7f4fa21150957
simonppg <simonppg@gmail.com>
2016-05-08 13:02:28 -0500

    Subiendo proyecto

---
+++ app/app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store from 'store';
+
+//Layouts
+import App from 'layouts/app';
+
+//Components
+import Home from 'ui/home';
+import UsersContainer from 'ui/users-container';
+
+ReactDOM.render((
+	<Provider store={store}>
+	<Router history={browserHistory}>
+		<Route component={App}>
+			<Route path="/" component={Home} />
+			<Route path="/users" component={UsersContainer} />
+		</Route>
+	</Router>
+	</Provider>
+), document.getElementById('root'));
